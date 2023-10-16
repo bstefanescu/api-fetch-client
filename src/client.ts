2c9ea28675ea7ffb10291686e5cb940e5d703682
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2023-10-16 20:50:21 +0200

    ApiTopic must reuse the parent client onRequest

--- src/client.ts
+++ src/client.ts
@@ -59,6 +59,10 @@ export abstract class ApiTopic extends ClientBase {
         super(client.getUrl(basePath), client._fetch);
     }
 
+    onRequest(init: RequestInit): void {
+        this.client.onRequest(init);
+    }
+
     get headers() {
         return this.client.headers;
     }
