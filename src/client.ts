f078b430e3b5065c4064ab4bc57715c921ba8714
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2023-10-26 21:34:56 +0200

    ApiTopic is losing auth context

--- src/client.ts
+++ src/client.ts
@@ -96,7 +96,7 @@ export abstract class ApiTopic extends ClientBase {
         super(client.getUrl(basePath), client._fetch);
     }
 
-    doRequest(fetch: FETCH_FN, url: string, init: RequestInit): Promise<Response> {
+    handleRequest(fetch: FETCH_FN, url: string, init: RequestInit): Promise<Response> {
         return this.client.handleRequest(fetch, url, init);
     }
 
